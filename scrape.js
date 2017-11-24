ebfaedd70defe26a046a2e43f0ec75bf4ff2dc3d
Bowden <wilkelly@microsoft.com>
2017-11-23 16:11:33 -0800

    sorta working but not really... think I'm going to switch to mongoose

--- scrape.js
+++ scrape.js
@@ -2,6 +2,23 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var request = require("request");
 var cheerio = require("cheerio");
+var bluebird_1 = require("bluebird");
+var config = {
+    DOCDB_HOST: process.env.DOCDB_HOST || 'https://betting-lines.documents.azure.com:443/',
+    DOCDB_AUTH_KEY: process.env.DOCDB_AUTH_KEY || 'WTznEcBUSpV1zKuvSNidYMBd1yuoHJrOR2A0N08DH8gTQOyzShCGWIhf5vhfab4A2aHjbBHEl87JfJbxByAV8g==',
+    DATABASE: process.env.DATABASE || 'Betting',
+    COLLECTION: process.env.COLLECTION || 'Games'
+};
+var docDBClient = require('documentdb').DocumentClient;
+var docDBConfig = {
+    "host": config.DOCDB_HOST,
+    "authKey": config.DOCDB_AUTH_KEY,
+    "database": { "id": config.DATABASE },
+    "collection": { "id": config.COLLECTION }
+};
+var databaseUrl = "dbs/" + docDBConfig.database.id;
+var collectionUrl = databaseUrl + "/colls/" + docDBConfig.collection.id;
+var docDB = new docDBClient(docDBConfig.host, { masterKey: docDBConfig.authKey });
 var Game = /** @class */ (function () {
     function Game(homeTeam, awayTeam, homeScore, awayScore, lines) {
         this.homeTeam = homeTeam;
@@ -9,6 +26,7 @@ var Game = /** @class */ (function () {
         this.homeScore = homeScore;
         this.awayScore = awayScore;
         this.lines = lines;
+        this.id = homeTeam + " vs. " + awayTeam;
     }
     return Game;
 }());
@@ -42,6 +60,55 @@ request(url, function (err, resp, body) {
             games.push(new Game(homeTeam, awayTeam, homeScore, awayScore, lines));
         }
     });
+    //add games to database
+    getDatabase().then(function (db) {
+        getCollection().then(function (collection) {
+            var _loop_1 = function (game) {
+                docDB.createDocument(collection._self, game, function (err, document) {
+                    if (err) {
+                        console.log('Error: createDocument: ' + err);
+                    }
+                    else {
+                        console.log('Success: ' + game.id);
+                    }
+                });
+            };
+            for (var _i = 0, games_1 = games; _i < games_1.length; _i++) {
+                var game = games_1[_i];
+                _loop_1(game);
+            }
+        }).catch(function (err) {
+            console.log('Error: getCollection: ' + err);
+        });
+    }).catch(function (err) {
+        console.log('Error: getDatabase: ' + err);
+    });
     console.log(games);
 });
+function getDatabase() {
+    console.log("Getting database: " + docDBConfig.database.id);
+    return new bluebird_1.Promise(function (resolve, reject) {
+        docDB.readDatabase(databaseUrl, function (err, result) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(result);
+            }
+        });
+    });
+}
+function getCollection() {
+    console.log("Getting collection: " + docDBConfig.collection.id);
+    return new bluebird_1.Promise(function (resolve, reject) {
+        docDB.readCollection(collectionUrl, function (err, result) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(result);
+            }
+        });
+    });
+}
 //# sourceMappingURL=scrape.js.map
--- scrape.ts
+++ scrape.ts
@@ -1,6 +1,26 @@
 import * as request from 'request';
 import * as cheerio from 'cheerio';
+import {Promise} from 'bluebird';
 
+const config = {
+    DOCDB_HOST: process.env.DOCDB_HOST || 'https://betting-lines.documents.azure.com:443/',
+    DOCDB_AUTH_KEY: process.env.DOCDB_AUTH_KEY || 'WTznEcBUSpV1zKuvSNidYMBd1yuoHJrOR2A0N08DH8gTQOyzShCGWIhf5vhfab4A2aHjbBHEl87JfJbxByAV8g==',
+    DATABASE: process.env.DATABASE || 'Betting',
+    COLLECTION: process.env.COLLECTION || 'Games'
+}
+
+var docDBClient = require('documentdb').DocumentClient;
+var docDBConfig = {
+    "host": config.DOCDB_HOST,
+    "authKey": config.DOCDB_AUTH_KEY,
+    "database": { "id": config.DATABASE },
+    "collection": { "id": config.COLLECTION }
+};
+
+var databaseUrl = `dbs/${docDBConfig.database.id}`;
+var collectionUrl = `${databaseUrl}/colls/${docDBConfig.collection.id}`;
+
+var docDB = new docDBClient(docDBConfig.host, { masterKey: docDBConfig.authKey });
 
 interface Line {
     name: string,
@@ -11,13 +31,16 @@ interface Line {
 }
 
 class Game {
+    id: string;
     constructor(
         public homeTeam: string, 
         public awayTeam: string, 
         public homeScore: number, 
         public awayScore: number, 
         public lines: Line[]
-    ) {}
+    ) {
+        this.id = homeTeam + " vs. " + awayTeam;
+    }
 }
 
 var url = 'http://www.donbest.com/nfl/odds/spreads/20171119.html';
@@ -25,7 +48,7 @@ var url = 'http://www.donbest.com/nfl/odds/spreads/20171119.html';
 request(url, function (err, resp, body) {
     if (err) { throw err; }
     let $ = cheerio.load(body);
-    let games = [];
+    let games: Game[] = [];
     $('tr').each(function() {
         const data = $(this).text();
         
@@ -53,5 +76,44 @@ request(url, function (err, resp, body) {
             games.push(new Game(homeTeam, awayTeam, homeScore, awayScore, lines));
         }
     });
+
+    //add games to database
+    getDatabase().then((db) => {
+        getCollection().then((collection:any) => {
+            for (let game of games) {
+                docDB.createDocument(collection._self, game, function (err, document) {
+                    if (err) {
+                        console.log('Error: createDocument: ' + err);
+                    } else {
+                        console.log('Success: ' + game.id);
+                    }
+                });
+            }
+        }).catch((err) => {
+            console.log('Error: getCollection: ' + err);
+        });
+    }).catch((err) => {
+        console.log('Error: getDatabase: ' + err);
+    });
     console.log(games);
 });
+
+function getDatabase() {
+    console.log(`Getting database: ${docDBConfig.database.id}`);
+    return new Promise((resolve, reject) => {
+        docDB.readDatabase(databaseUrl, (err, result) => {
+            if (err) { reject(err); }
+            else { resolve(result); }
+        });
+    });
+}
+
+function getCollection() {
+    console.log(`Getting collection: ${docDBConfig.collection.id}`);
+    return new Promise((resolve, reject) => {
+        docDB.readCollection(collectionUrl, (err, result) => {
+            if (err) { reject(err); } 
+            else { resolve(result); }
+        });
+    });
+}

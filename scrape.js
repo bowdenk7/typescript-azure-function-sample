53c20d51dcd6fc9376abf7e0c1c65580f91d57e1
Bowden <wilkelly@microsoft.com>
2018-01-30 18:24:11 -0800

    oops

--- scrape.js
+++ scrape.js
@@ -1,112 +1,142 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var request = require("request");
-var cheerio = require("cheerio");
-var bluebird_1 = require("bluebird");
-var config = {
-    DOCDB_HOST: process.env.DOCDB_HOST || 'https://betting-lines.documents.azure.com:443/',
-    DOCDB_AUTH_KEY: process.env.DOCDB_AUTH_KEY || 'WTznEcBUSpV1zKuvSNidYMBd1yuoHJrOR2A0N08DH8gTQOyzShCGWIhf5vhfab4A2aHjbBHEl87JfJbxByAV8g==',
-    DATABASE: process.env.DATABASE || 'Betting',
-    COLLECTION: process.env.COLLECTION || 'Games'
-};
-var docDBClient = require('documentdb').DocumentClient;
-var docDBConfig = {
-    "host": config.DOCDB_HOST,
-    "authKey": config.DOCDB_AUTH_KEY,
-    "database": { "id": config.DATABASE },
-    "collection": { "id": config.COLLECTION }
-};
-var databaseUrl = "dbs/" + docDBConfig.database.id;
-var collectionUrl = databaseUrl + "/colls/" + docDBConfig.collection.id;
-var docDB = new docDBClient(docDBConfig.host, { masterKey: docDBConfig.authKey });
-var Game = /** @class */ (function () {
-    function Game(homeTeam, awayTeam, homeScore, awayScore, lines) {
+const rp = require("request-promise");
+const cheerio = require("cheerio");
+const moment = require("moment");
+var util = require('util');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+// local
+//const mongoUri = 'mongodb://localhost/Betting';
+//heroku
+const mongoUri = "mongodb://bowden:bowden@ds121456.mlab.com:21456/heroku_27vbtgss";
+mongoose.set('debug', true);
+const gameSchema = new mongoose.Schema({
+    date: Date,
+    dateString: String,
+    homeTeam: String,
+    awayTeam: String,
+    homeScore: String,
+    awayScore: String,
+    lines: Array
+}, {
+    collection: 'Games'
+});
+const GameModel = mongoose.model('Game', gameSchema);
+class Game {
+    constructor(date, dateString, homeTeam, awayTeam, homeScore, awayScore, lines) {
+        this.date = date;
+        this.dateString = dateString;
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
         this.homeScore = homeScore;
         this.awayScore = awayScore;
         this.lines = lines;
-        this.id = homeTeam + " vs. " + awayTeam;
+        //this.id = homeTeam + " vs. " + awayTeam;
     }
-    return Game;
-}());
-var url = 'http://www.donbest.com/nfl/odds/spreads/20171119.html';
-request(url, function (err, resp, body) {
-    if (err) {
-        throw err;
-    }
-    var $ = cheerio.load(body);
-    var games = [];
-    $('tr').each(function () {
-        var data = $(this).text();
-        if ($(this)[0].children[2] && !($(this)[0].children[2].children[0].data == "Team")) {
-            var awayTeam = $(this)[0].children[2].children[0].children[0].children[0].children[0].data;
-            var homeTeam = $(this)[0].children[2].children[0].children[2].children[0].children[0].data;
-            var awayScore = parseFloat($(this)[0].children[4].children[0].children[0].children[0].data);
-            var homeScore = parseFloat($(this)[0].children[4].children[1].children[0].children[0].data);
-            var lineNames = ["Westgate", "Mirage", "Station", "Pinnacle", "SIA", "SBG", "BetUS", "BetPhoenix",
-                "EasyStreet", "Bovada", "Jazz", "Sportsbet", "BookMaker", "DSI", "AceSport"];
-            var lines = [];
-            for (var i = 0; i < lineNames.length; i++) {
-                var lineData = {
-                    name: lineNames[i],
-                    awayLine: parseFloat($(this)[0].children[6 + i].children[0].children[0].data.split("\n")[0]),
-                    awayOdds: parseFloat($(this)[0].children[6 + i].children[0].children[0].data.split("\n")[1].trim()),
-                    homeline: parseFloat($(this)[0].children[6 + i].children[1].children[0].data.split("\n")[0]),
-                    homeOdds: parseFloat($(this)[0].children[6 + i].children[1].children[0].data.split("\n")[1].trim())
-                };
-                lines.push(lineData);
-            }
-            games.push(new Game(homeTeam, awayTeam, homeScore, awayScore, lines));
+}
+var baseUrl = 'http://www.donbest.com/nfl/odds/spreads/';
+function scrapeBettingLines(dates) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let allGames = [];
+        for (let date of dates) {
+            let url = baseUrl + date + ".html";
+            let games = yield scrapeGameLinesFromUrl(url, date);
+            allGames = allGames.concat(games);
         }
-    });
-    //add games to database
-    getDatabase().then(function (db) {
-        getCollection().then(function (collection) {
-            var _loop_1 = function (game) {
-                docDB.createDocument(collection._self, game, function (err, document) {
-                    if (err) {
-                        console.log('Error: createDocument: ' + err);
-                    }
-                    else {
-                        console.log('Success: ' + game.id);
-                    }
-                });
-            };
-            for (var _i = 0, games_1 = games; _i < games_1.length; _i++) {
-                var game = games_1[_i];
-                _loop_1(game);
-            }
-        }).catch(function (err) {
-            console.log('Error: getCollection: ' + err);
-        });
-    }).catch(function (err) {
-        console.log('Error: getDatabase: ' + err);
-    });
-    console.log(games);
-});
-function getDatabase() {
-    console.log("Getting database: " + docDBConfig.database.id);
-    return new bluebird_1.Promise(function (resolve, reject) {
-        docDB.readDatabase(databaseUrl, function (err, result) {
-            if (err) {
-                reject(err);
-            }
-            else {
-                resolve(result);
-            }
-        });
+        console.log("Done scraping.");
+        console.log(`Saving ${allGames.length} games to database`);
+        yield saveGames(allGames);
+        console.log("All done.");
     });
 }
-function getCollection() {
-    console.log("Getting collection: " + docDBConfig.collection.id);
-    return new bluebird_1.Promise(function (resolve, reject) {
-        docDB.readCollection(collectionUrl, function (err, result) {
-            if (err) {
-                reject(err);
+let dates = getDateStrings('20171124', 30);
+console.log(`Fetching scores and lines from ${prettyPrintDate(dates[dates.length - 1])} to ${prettyPrintDate(dates[0])}`);
+scrapeBettingLines(dates);
+/**
+ *
+ * @param startDate string date in the format: YYYYMMDD eg: '20171119'
+ * @param numberDays number of days fetch counting backward from start date
+ */
+function getDateStrings(startDate, numberDays) {
+    let dates = [];
+    for (let i = 0; i < numberDays + 1; i++) {
+        dates.push(moment(startDate, "YYYYMMDD").subtract(i, 'days').format("YYYYMMDD"));
+    }
+    let year = startDate;
+    let month = startDate;
+    let day = startDate;
+    return dates;
+}
+/**
+ * Prints a YYYYMMDD date as DD-MM-YYYY
+ */
+function prettyPrintDate(date) {
+    return moment(date, "YYYYMMDD").format("MM-DD-YYYY");
+}
+function scrapeGameLinesFromUrl(url, date) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log("Fetching data from: " + url);
+        let body = yield rp(url);
+        let $ = cheerio.load(body);
+        let games = [];
+        $('tr').each(function () {
+            const data = $(this).text();
+            if ($(this)[0].children[2] && !($(this)[0].children[2].children[0].data == "Team")) {
+                const awayTeam = $(this)[0].children[2].children[0].children[0].children[0].children[0].data;
+                const homeTeam = $(this)[0].children[2].children[0].children[2].children[0].children[0].data;
+                const awayScore = parseFloat($(this)[0].children[4].children[0].children[0].children[0].data);
+                const homeScore = parseFloat($(this)[0].children[4].children[1].children[0].children[0].data);
+                const lineNames = ["Westgate", "Mirage", "Station", "Pinnacle", "SIA", "SBG", "BetUS", "BetPhoenix",
+                    "EasyStreet", "Bovada", "Jazz", "Sportsbet", "BookMaker", "DSI", "AceSport"];
+                let lines = [];
+                for (let i = 0; i < lineNames.length; i++) {
+                    let lineData = {
+                        name: lineNames[i],
+                        awayLine: parseFloat($(this)[0].children[6 + i].children[0].children[0].data.split("\n")[0]),
+                        awayOdds: parseFloat($(this)[0].children[6 + i].children[0].children[0].data.split("\n")[1].trim()),
+                        homeline: parseFloat($(this)[0].children[6 + i].children[1].children[0].data.split("\n")[0]),
+                        homeOdds: parseFloat($(this)[0].children[6 + i].children[1].children[0].data.split("\n")[1].trim())
+                    };
+                    lines.push(lineData);
+                }
+                let momentDate = moment(date, "YYYYMMDD");
+                games.push(new Game(momentDate.toDate(), momentDate.format("MM-DD-YYYY"), homeTeam, awayTeam, homeScore, awayScore, lines));
             }
-            else {
-                resolve(result);
+        });
+        return games;
+    });
+}
+function saveGames(games) {
+    return __awaiter(this, void 0, void 0, function* () {
+        mongoose.connect(mongoUri, { useMongoClient: true }, function (err) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log("Client DB: connected");
+            for (let game of games) {
+                let gameModel = new GameModel({
+                    homeTeam: game.homeTeam,
+                    awayTeam: game.awayTeam,
+                    homeScore: game.homeScore,
+                    awayScore: game.awayScore,
+                    lines: game.lines
+                });
+                gameModel.save((err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log('done');
+                });
             }
         });
     });
--- scrape.ts
+++ scrape.ts
@@ -1,26 +1,36 @@
-import * as request from 'request';
+import * as rp from 'request-promise';
 import * as cheerio from 'cheerio';
-import {Promise} from 'bluebird';
+import { start } from 'repl';
+import * as moment from 'moment';
+var util = require('util');
 
-const config = {
-    DOCDB_HOST: process.env.DOCDB_HOST || 'https://betting-lines.documents.azure.com:443/',
-    DOCDB_AUTH_KEY: process.env.DOCDB_AUTH_KEY || 'WTznEcBUSpV1zKuvSNidYMBd1yuoHJrOR2A0N08DH8gTQOyzShCGWIhf5vhfab4A2aHjbBHEl87JfJbxByAV8g==',
-    DATABASE: process.env.DATABASE || 'Betting',
-    COLLECTION: process.env.COLLECTION || 'Games'
-}
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
-var docDBClient = require('documentdb').DocumentClient;
-var docDBConfig = {
-    "host": config.DOCDB_HOST,
-    "authKey": config.DOCDB_AUTH_KEY,
-    "database": { "id": config.DATABASE },
-    "collection": { "id": config.COLLECTION }
-};
+// local
+//const mongoUri = 'mongodb://localhost/Betting';
 
-var databaseUrl = `dbs/${docDBConfig.database.id}`;
-var collectionUrl = `${databaseUrl}/colls/${docDBConfig.collection.id}`;
+//heroku
+const mongoUri = "";
 
-var docDB = new docDBClient(docDBConfig.host, { masterKey: docDBConfig.authKey });
+mongoose.set('debug', true);
+
+const gameSchema = new mongoose.Schema(
+    {
+        date: Date,
+        dateString: String,
+        homeTeam: String,
+        awayTeam: String,
+        homeScore: String,
+        awayScore: String,
+        lines: Array
+    },
+    {
+        collection: 'Games'
+    }
+);
+
+const GameModel = mongoose.model('Game', gameSchema);
 
 interface Line {
     name: string,
@@ -33,32 +43,81 @@ interface Line {
 class Game {
     id: string;
     constructor(
+        public date: Date,
+        public dateString: string,
         public homeTeam: string, 
         public awayTeam: string, 
         public homeScore: number, 
         public awayScore: number, 
         public lines: Line[]
     ) {
-        this.id = homeTeam + " vs. " + awayTeam;
+        //this.id = homeTeam + " vs. " + awayTeam;
     }
 }
 
-var url = 'http://www.donbest.com/nfl/odds/spreads/20171119.html';
+var baseUrl = 'http://www.donbest.com/nfl/odds/spreads/';
 
-request(url, function (err, resp, body) {
-    if (err) { throw err; }
+async function scrapeBettingLines(dates) {
+    let allGames: Game[] = [];
+    for (let date of dates) {
+        let url = baseUrl + date + ".html";
+        let games = await scrapeGameLinesFromUrl(url, date);
+        allGames = allGames.concat(games);
+    }
+    console.log("Done scraping.");
+
+    console.log(`Saving ${allGames.length} games to database`);
+    await saveGames(allGames);
+    console.log("All done.");
+}
+
+let dates = getDateStrings('20171124', 30);
+console.log(`Fetching scores and lines from ${prettyPrintDate(dates[dates.length - 1])} to ${prettyPrintDate(dates[0])}`)
+
+scrapeBettingLines(dates);
+
+
+/**
+ * 
+ * @param startDate string date in the format: YYYYMMDD eg: '20171119'
+ * @param numberDays number of days fetch counting backward from start date
+ */
+function getDateStrings(startDate, numberDays) {
+    let dates: string[] = [];
+
+    for (let i = 0; i < numberDays+1; i++) {
+        dates.push(moment(startDate, "YYYYMMDD").subtract(i, 'days').format("YYYYMMDD"));
+    }
+    
+    let year = startDate;
+    let month = startDate;
+    let day = startDate;
+
+
+    return dates;
+}
+
+/**
+ * Prints a YYYYMMDD date as DD-MM-YYYY
+ */
+function prettyPrintDate(date: string) {
+    return moment(date, "YYYYMMDD").format("MM-DD-YYYY");
+}
+
+async function scrapeGameLinesFromUrl(url: string, date: string): Promise<Game[]> {
+    console.log("Fetching data from: " + url);
+    let body: any = await rp(url);
+    
     let $ = cheerio.load(body);
     let games: Game[] = [];
-    $('tr').each(function() {
+    $('tr').each(function () {
         const data = $(this).text();
-        
+
         if ($(this)[0].children[2] && !($(this)[0].children[2].children[0].data == "Team")) { //ghetto way of detecting that our row is not a header
             const awayTeam = $(this)[0].children[2].children[0].children[0].children[0].children[0].data;
             const homeTeam = $(this)[0].children[2].children[0].children[2].children[0].children[0].data;
-
             const awayScore = parseFloat($(this)[0].children[4].children[0].children[0].children[0].data);
             const homeScore = parseFloat($(this)[0].children[4].children[1].children[0].children[0].data);
-
             const lineNames = ["Westgate", "Mirage", "Station", "Pinnacle", "SIA", "SBG", "BetUS", "BetPhoenix",
                 "EasyStreet", "Bovada", "Jazz", "Sportsbet", "BookMaker", "DSI", "AceSport"];
             let lines: Line[] = [];
@@ -73,47 +132,34 @@ request(url, function (err, resp, body) {
 
                 lines.push(lineData);
             }
-            games.push(new Game(homeTeam, awayTeam, homeScore, awayScore, lines));
+            let momentDate = moment(date, "YYYYMMDD");
+            games.push(new Game(momentDate.toDate(), momentDate.format("MM-DD-YYYY"), homeTeam, awayTeam, homeScore, awayScore, lines));
         }
     });
-
-    //add games to database
-    getDatabase().then((db) => {
-        getCollection().then((collection:any) => {
-            for (let game of games) {
-                docDB.createDocument(collection._self, game, function (err, document) {
-                    if (err) {
-                        console.log('Error: createDocument: ' + err);
-                    } else {
-                        console.log('Success: ' + game.id);
-                    }
-                });
-            }
-        }).catch((err) => {
-            console.log('Error: getCollection: ' + err);
-        });
-    }).catch((err) => {
-        console.log('Error: getDatabase: ' + err);
-    });
-    console.log(games);
-});
-
-function getDatabase() {
-    console.log(`Getting database: ${docDBConfig.database.id}`);
-    return new Promise((resolve, reject) => {
-        docDB.readDatabase(databaseUrl, (err, result) => {
-            if (err) { reject(err); }
-            else { resolve(result); }
-        });
-    });
+    return games;
 }
 
-function getCollection() {
-    console.log(`Getting collection: ${docDBConfig.collection.id}`);
-    return new Promise((resolve, reject) => {
-        docDB.readCollection(collectionUrl, (err, result) => {
-            if (err) { reject(err); } 
-            else { resolve(result); }
-        });
+async function saveGames(games: Game[]) {
+    mongoose.connect(mongoUri, { useMongoClient: true }, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log("Client DB: connected");
+        for (let game of games) {
+            let gameModel = new GameModel({
+                homeTeam: game.homeTeam,
+                awayTeam: game.awayTeam,
+                homeScore: game.homeScore,
+                awayScore: game.awayScore,
+                lines: game.lines
+            });
+            gameModel.save((err) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log('done');
+            });
+        }
     });
 }
